168221600cc9db770fac80ed3e6e954c66757769
4levy <100963276+4levy@users.noreply.github.com>
2025-01-26 00:20:12 +0700

    Add files via upload

---
+++ setup/starter.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tk: [
+    "ODc0ODk4NDIyMjMzMTc4MTQy.", // Token 1
+    "MTE5MTM4NDQ3MzM4MDU4OTYwOA", // Token 2 (Can be deleted if not needed)
+  ],
+
+  config: require("./config.json"),
+};
